35f59fb21f53557edd75c0534817627e0ca99562
Jose1i1o <guadix88@gmail.com>
2021-09-17 21:19:49 +0200

    feat(titles and images): display basic functionality

---
+++ films-web/src/App.jsx
@@ -0,0 +1,14 @@
+import { MoviesGrid } from "./MoviesGrid";
+
+export function App() {
+  return (
+    <div>
+      <header>
+        <h1>The most watched films</h1>
+      </header>
+      <main>
+        <MoviesGrid />
+      </main>
+    </div>
+  );
+}
